4709ff785691c944554b3cd9f4d216d239c0ed7f
pedro pablo malagon amor <pedropabloma@msn.com>
2016-06-17 10:25:36 +0200

    Add project files.

---
+++ BotAndLuisDemo/TwitterDialog.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tweetinvi.Core.Interfaces;
+
+namespace BotAndLuisDemo
+{
+    [LuisModel("fa71c414-2110-4ce9-b258-67e36f3cf3e8", "25d92877ee5e4f06be37a75607d3eade")]
+    [Serializable]
+    public class TwitterDialog : LuisDialog<object>
+    {
+        [LuisIntent("")] //None
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry I did not understand. Could you repeat?";
+            await context.PostAsync(message);
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Greetings")]
+        public async Task Greetings(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Como estas ! , estan son mis opciones : ");
+            await context.PostAsync("puedo buscar un perfil en Twitter, utiliza: 'busca'");
+            await context.PostAsync("y puedo buscar en hashtags, utiliza: 'que pasa sobre'");
+            await context.PostAsync("ah, además se decir 'Hola' :-)");
+            // await context.PostAsync("!(http://aka.ms/Fo983c)");
+            // await context.PostAsync("Cuac!");
+            // await context.PostAsync("![duck](http://img.glimboo.com/smiles/0011.gif)");
+
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("SearchUser")]
+        public async Task SearchUser(IDialogContext context, LuisResult result)
+        {
+            if (result.Entities.Count > 0)
+            {
+
+                var userName = result.Entities[0].Entity;
+
+                try
+                {
+                    await context.PostAsync(string.Format("This is what **{0}** says: ", userName));
+                    var user = TwitterService.GetUser(userName);
+                    await context.PostAsync(string.Format("![userName]({0})", user.ProfileImageUrl400x400));
+                    await context.PostAsync(user.Description);
+
+                }
+                catch (Exception ex)
+                {
+                    await context.PostAsync(ex.Message);
+                }
+            }
+            else
+            {
+                await context.PostAsync("Wait, what?");
+            }
+
+            context.Wait(MessageReceived);
+        }
+
+
+        [LuisIntent("SearchKeyword")]
+        public async Task SearchKeyword(IDialogContext context, LuisResult result)
+        {
+            if (result.Entities.Count > 0)
+            {
+
+                IEnumerable<ITweet> tweets = TwitterService.GetKeyword(result.Entities[0].Entity);
+
+                foreach (var tweet in tweets)
+                {
+                    await context.PostAsync(tweet.Text);
+                }
+            }
+            else
+            {
+                await context.PostAsync("Wait what?");
+            }
+
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ BotAndLuisDemo/TwitterService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces;
+
+namespace BotAndLuisDemo
+{
+    public static class TwitterService
+    {
+        static TwitterService()
+        {
+            const string consumerKey = "e037q3on2cD7hF7lKZq8sxGlt"; // The application's consumer key
+            const string consumerSecret = "rfDAKEUcjH0J3XQJwMkh8xp1S7J9CUa5b3XKvas29ATfzeO9hJ"; // The application's consumer secret
+            const string accessToken = "51491702-L4XExU1N71rZtyfYrXm8jTBUHxtlfYDUmpQ4DY6GB"; // The access token granted after OAuth authorization
+            const string accessTokenSecret = "6xtYhJ7y3RjQ2yAIhepVYjdZQjYizOQ58tXwFljaEVIGb"; // The access token secret granted after OAuth authorization
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+        }
+
+        public static IUser GetUser(string name)
+        {
+            return User.GetUserFromScreenName(name);
+        }
+
+        public static IEnumerable<ITweet> GetKeyword(string keyword)
+        {
+
+            return Search.SearchTweets(keyword).Take(30);
+            //return Search.SearchTweets(keyword).Take(3);
+        }
+    }
+}
